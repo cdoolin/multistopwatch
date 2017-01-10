d40ff339a5aa534b840afa2461ac1615e7063af8
ri-lab <callumolvedinstruments.com>
2017-01-10 11:45:23 -0700

    stored stopwatches in array to make changing number of watches easier in the future

--- multistopwatch.js
+++ multistopwatch.js
@@ -1,90 +1,61 @@
 $(function () {
-    var t0 = 0;
-    var tt0 = 0;
-    
-    function timer0() {
-        if (t0 == 0) {
-            t0 = Date.now();
+    // reference time to count stopwatch from
+    // if ts[i] is not 0, means stopwatch i is running
+    var t0s = [0, 0, 0];
+    // total time on stopwatch.
+    var tts = [0, 0, 0];
+    // key codes to start/stop stopwatches
+    var keys = [113, 119, 101];
+    var nstopwatches = 3;
+
+
+    function timer(stopwatch) {
+        if (t0s[stopwatch] == 0) {
+            // stopwatch is stopped,  start it
+            t0s[stopwatch] = Date.now();
         }
         else {
-            tt0 += Date.now() - t0;
-            t0 = 0;
+            // stopwatch is going, record time and stop it.
+            tts[stopwatch] += Date.now() - t0s[stopwatch];
+            t0s[stopwatch] = 0;
         }
     }
-    
-    var t1 = 0;
-    var tt1 = 0;
-    
-    function timer1() {
-        if (t1 == 0) {
-            t1 = Date.now();
-        }
-        else {
-            tt1 += Date.now() - t1;
-            t1 = 0;
-        }
-    }
-    
-    var t2 = 0;
-    var tt2 = 0;
-    
-    function timer2() {
-        if (t2 == 0) {
-            t2 = Date.now();
-        }
-        else {
-            tt2 += Date.now() - t2;
-            t2 = 0;
-        }
-    }
-    
+
+    $("#button0").click(function() {timer(0);});
+    $("#button1").click(function() {timer(1);});
+    $("#button2").click(function() {timer(2);});
+
+
     function reset() {
-        t0 = 0;
-        tt0 = 0;
-        t1 = 0;
-        tt1 = 0;
-        t2 = 0;
-        tt2 = 0;
+        for (var i = 0; i < nstopwatches; i++) {
+            t0s[i] = 0;
+            tts[i] = 0;
+        }
     }
-    
-    $("#button0").click(timer0);
-    $("#button1").click(timer1);
-    $("#button2").click(timer2);
-    
     $("#resetbutton").click(reset);
-    
+
+
     $(document).keypress(function (event) {
-        if (event.charCode == 113)
-            timer0();
-        if (event.charCode == 119)
-            timer1();
-        if (event.charCode == 101)
-            timer2();
+        for (var i = 0; i < nstopwatches; i++) {
+            if (event.charCode == keys[i]) {
+                timer(i);
+                break;
+            }
+        }
+
         if (event.charCode == 114)
             reset();
     });
-    
-    
+
+
     window.setInterval(function() {
-        var time = tt0;
-        if (t0 > 0) {
-            time += Date.now() - t0;
+        for (var i = 0; i < nstopwatches; i++) {
+            var time = tts[i];
+            if (t0s[i] > 0) {
+                time += Date.now() - t0s[i];
+            }
+            time /= 1000;
+            $("#time" + i).text(time.toFixed(3));
         }
-        time /= 1000;
-        $("#time0").text(time.toFixed(3));
-        
-        var time = tt1;
-        if (t1 > 0) {
-            time += Date.now() - t1;
-        }
-        time /= 1000;
-        $("#time1").text(time.toFixed(3));
-        
-        var time = tt2;
-        if (t2 > 0) {
-            time += Date.now() - t2;
-        }
-        time /= 1000;
-        $("#time2").text(time.toFixed(3));
     }, 80);
 });
